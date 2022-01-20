d512e787c2a1b091b174a4a9f898d26483b30691
phann-phearoen <74291680+phann-phearoen@users.noreply.github.com>
2022-01-20 16:41:09 +0700

    add tiptap with no trouble

---
+++ components/Tiptap.vue
@@ -0,0 +1,48 @@
+<template>
+    <editor-content :editor="editor" />
+</template>
+
+<script>
+import { Editor, EditorContent } from '@tiptap/vue-2'
+import StarterKit from '@tiptap/starter-kit'
+import Placeholder from '@tiptap/extension-placeholder'
+
+export default {
+    components: {
+        EditorContent,
+    },
+
+    data() {
+        return {
+        editor: null,
+        }
+    },
+
+    mounted() {
+        this.editor = new Editor({
+            content: '',
+            extensions: [
+                StarterKit,
+                Placeholder.configure({
+                    placeholder: 'write something...',
+                }),
+                
+            ],
+        })
+    },
+
+    beforeDestroy() {
+        this.editor.destroy()
+    },
+}
+</script>
+
+<style scoped>
+/* Placeholder (at the top) */
+.ProseMirror p.is-editor-empty:first-child::before {
+  content: attr(data-placeholder);
+  float: left;
+  color: #adb5bd;
+  pointer-events: none;
+}
+</style>>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,27 @@
 <template>
-  <v-container>
-	  <v-card>
-		  <v-card-title>Try quill by cdn</v-card-title>
-		  <v-divider></v-divider>
-		  <v-form>
-			  <v-text-field
-			  label="Field 1"
-			  v-model="field1"
-			  ></v-text-field>
-			  <v-text-field
-			  label="Field 2"
-			  v-model="field2"
-			  ></v-text-field>
+	<v-container>
+		<v-card>
+			<v-card-title>Try quill by cdn</v-card-title>
+			<v-divider></v-divider>
+			<v-form>
+				<v-text-field
+				label="Field 1"
+				v-model="field1"
+				></v-text-field>
+				<v-text-field
+				label="Field 2"
+				v-model="field2"
+				></v-text-field>
 
-			  <div id="editor"></div>
-		  </v-form>
-	  </v-card>
-  </v-container>
+				<client-only>
+					<tiptap />
+				</client-only>
+			</v-form>
+		</v-card>
+	</v-container>
 </template>
 
 <script>
-import { Quill } from 'quill'
-
 export default {
   	name: 'IndexPage',
 	data() {
@@ -30,13 +30,5 @@ export default {
 			field2: "",
 		}
 	},
-	mounted() {
-		var quill = new Quill('#editor', {
-			modules: {
-				// Equivalent to { toolbar: { container: '#toolbar' }}
-				toolbar: '#toolbar'
-			}
-		});
-	}
 }
 </script>
