24fc9708fc8a5986902c3014e5eb0dc9c2e8dfda
phann-phearoen <74291680+phann-phearoen@users.noreply.github.com>
2022-01-21 09:07:40 +0700

    add menubar

--- components/Tiptap.vue
+++ components/Tiptap.vue
@@ -1,5 +1,71 @@
 <template>
-    <editor-content :editor="editor" />
+    <v-card outlined>
+        <v-row
+        class="pb-2 ma-0"
+        justify="space-between"
+        >
+            <v-card
+            elevation="0"
+            outlined
+            color="#F5F5F5"
+            width="100%"
+            >
+                <v-btn-toggle
+                    v-model="formatting"
+                    multiple
+                >
+                    <v-btn>
+                        <v-icon>mdi-format-italic</v-icon>
+                    </v-btn>
+
+                    <v-btn>
+                        <v-icon>mdi-format-bold</v-icon>
+                    </v-btn>
+
+                    <v-btn>
+                        <v-icon>mdi-format-underline</v-icon>
+                    </v-btn>
+
+                    <v-btn>
+                        <v-row
+                            align="center"
+                            class="flex-column"
+                            justify="center"
+                        >
+                            <v-icon class="cols 12">
+                            mdi-format-color-text
+                            </v-icon>
+
+                            <v-sheet
+                            tile
+                            style="margin-top: -4px;"
+                            height="4"
+                            width="26"
+                            color="purple"
+                            ></v-sheet>
+                        </v-row>
+                    </v-btn>
+                </v-btn-toggle>
+
+                <v-btn-toggle v-model="alignment">
+                    <v-btn>
+                        <v-icon>mdi-format-align-center</v-icon>
+                    </v-btn>
+
+                    <v-btn>
+                        <v-icon>mdi-format-align-left</v-icon>
+                    </v-btn>
+
+                    <v-btn>
+                        <v-icon>mdi-format-align-right</v-icon>
+                    </v-btn>
+                </v-btn-toggle>
+            </v-card>
+        </v-row>
+        <v-card-text>
+            <editor-content :editor="editor" />
+        </v-card-text>
+    </v-card>
 </template>
 
 <script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,20 +3,22 @@
 		<v-card>
 			<v-card-title>Try quill by cdn</v-card-title>
 			<v-divider></v-divider>
-			<v-form>
-				<v-text-field
-				label="Field 1"
-				v-model="field1"
-				></v-text-field>
-				<v-text-field
-				label="Field 2"
-				v-model="field2"
-				></v-text-field>
+			<v-card-text>
+				<v-form>
+					<v-text-field
+					label="Field 1"
+					v-model="field1"
+					></v-text-field>
+					<v-text-field
+					label="Field 2"
+					v-model="field2"
+					></v-text-field>
 
-				<client-only>
-					<tiptap />
-				</client-only>
-			</v-form>
+					<client-only>
+						<tiptap />
+					</client-only>
+				</v-form>
+			</v-card-text>
 		</v-card>
 	</v-container>
 </template>
