d65db0447fe5ea0c11fb3b5d17b77c6400f25d4a
phann-phearoen <74291680+phann-phearoen@users.noreply.github.com>
2022-01-22 14:46:20 +0700

    successfull add style to prosemirror

--- components/Tiptap.vue
+++ components/Tiptap.vue
@@ -1,77 +1,10 @@
 <template>
-    <v-card outlined>
-        <v-row
-        class="pb-2 ma-0"
-        justify="space-between"
-        >
-            <v-card
-            elevation="0"
-            outlined
-            color="#F5F5F5"
-            width="100%"
-            >
-                <v-btn-toggle
-                    v-model="formatting"
-                    multiple
-                >
-                    <v-btn>
-                        <v-icon>mdi-format-italic</v-icon>
-                    </v-btn>
-
-                    <v-btn>
-                        <v-icon>mdi-format-bold</v-icon>
-                    </v-btn>
-
-                    <v-btn>
-                        <v-icon>mdi-format-underline</v-icon>
-                    </v-btn>
-
-                    <v-btn>
-                        <v-row
-                            align="center"
-                            class="flex-column"
-                            justify="center"
-                        >
-                            <v-icon class="cols 12">
-                            mdi-format-color-text
-                            </v-icon>
-
-                            <v-sheet
-                            tile
-                            style="margin-top: -4px;"
-                            height="4"
-                            width="26"
-                            color="purple"
-                            ></v-sheet>
-                        </v-row>
-                    </v-btn>
-                </v-btn-toggle>
-
-                <v-btn-toggle v-model="alignment">
-                    <v-btn>
-                        <v-icon>mdi-format-align-center</v-icon>
-                    </v-btn>
-
-                    <v-btn>
-                        <v-icon>mdi-format-align-left</v-icon>
-                    </v-btn>
-
-                    <v-btn>
-                        <v-icon>mdi-format-align-right</v-icon>
-                    </v-btn>
-                </v-btn-toggle>
-            </v-card>
-        </v-row>
-        <v-card-text>
-            <editor-content :editor="editor" />
-        </v-card-text>
-    </v-card>
+    <editor-content :editor="editor" />
 </template>
 
 <script>
 import { Editor, EditorContent } from '@tiptap/vue-2'
 import StarterKit from '@tiptap/starter-kit'
-import Placeholder from '@tiptap/extension-placeholder'
 
 export default {
     components: {
@@ -83,16 +16,11 @@ export default {
             editor: null,
         }
     },
-
     mounted() {
         this.editor = new Editor({
             content: '',
             extensions: [
                 StarterKit,
-                Placeholder.configure({
-                    placeholder: 'write something...',
-                }),
-                
             ],
         })
     },
@@ -103,12 +31,10 @@ export default {
 }
 </script>
 
-<style scoped>
-/* Placeholder (at the top) */
-.ProseMirror p.is-editor-empty:first-child::before {
-  content: attr(data-placeholder);
-  float: left;
-  color: #adb5bd;
-  pointer-events: none;
+<style>
+.ProseMirror {
+    min-height: 200px;
+    border: 1.5px solid grey;
+    border-radius: 5px;
 }
-</style>>
+</style>
