343053c607cfabebba86534a9fa988522edf2440
phann-phearoen <74291680+phann-phearoen@users.noreply.github.com>
2022-01-29 14:12:06 +0700

    add tiptap image component to approach tiptap vue component rendering

---
+++ components/TiptapImage.vue
@@ -0,0 +1,22 @@
+<template>
+    <node-view-wrapper class="vue-component">
+        <div class="content">
+            <v-img :src="node.attrs.img" contain></v-img>
+        <!-- <button @click="increase">
+            This button has been clicked {{ node.attrs.count }} times.
+        </button> -->
+        </div>
+    </node-view-wrapper>
+</template>
+
+<script>
+import { NodeViewWrapper, nodeViewProps } from '@tiptap/vue-2'
+
+export default {
+    components: {
+        NodeViewWrapper,
+    },
+
+    props: nodeViewProps,
+}
+</script>
